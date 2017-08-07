ebfc78be07fbb20c5dd3306f1c01bbb237a22bc9
Roberth Mejias <piedrucci@gmail.com>
2017-08-07 10:42:16 -0500

    adding camera component

--- app.js
+++ app.js
@@ -1,40 +1,78 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry, StyleSheet, Text, View, AlertIOS
 } from 'react-native';
 
+import Camera from 'react-native-camera'
+
 class CameraComp extends Component {
+   constructor() {
+      super()
+      this.state = {
+         showCamera: true,
+         cameraType: Camera.constants.Type.back
+      }
+   }
+
+   _onBarCodeRead(e) {
+      this.setState({showCamera: false});
+      AlertIOS.alert(
+         "Barcode Found!",
+         "Type: " + e.type + "\nData: " + e.data
+      );
+   }
+
+   renderCamera() {
+      if(this.state.showCamera) {
+         return (
+            <Camera
+               ref="cam"
+               style={styles.container}
+               onBarCodeRead={this._onBarCodeRead}
+               type={this.state.cameraType}>
+            </Camera>
+         );
+      } else {
+         return (
+            <View style={styles.container}>
+               <Text style={styles.welcome}>
+                  React Native Camera iOS!
+               </Text>
+               {/* <Text style={styles.instructions}>
+                  To get started, edit index.ios.js
+               </Text>
+               <Text style={styles.instructions}>
+                  Press Cmd+R to reload,{'\n'}
+                  Cmd+D or shake for dev menu
+               </Text> */}
+            </View>
+         );
+      }
+   }
+
+
    render() {
       return (
-
-         <View style={styles.container}>
-            <Text style={styles.welcome}>
-               React Native Camera iOS!
-            </Text>
-            {/* <Text style={styles.instructions}>
-               To get started, edit index.ios.js
-            </Text>
-            <Text style={styles.instructions}>
-               Press Cmd+R to reload,{'\n'}
-               Cmd+D or shake for dev menu
-            </Text> */}
-         </View>
-
+         this.renderCamera()
       );
    }
 
 }
 
 const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+     flex: 1,
+     justifyContent: "center",
+     alignItems: "center",
+     backgroundColor: "transparent",
   },
+
   welcome: {
     fontSize: 20,
     textAlign: 'center',
